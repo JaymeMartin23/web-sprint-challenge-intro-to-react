8dbe4ebf8a684e58bb09331f32bc479584128b61
Jayme Martin <jaymelee70@gmail.com>
2023-04-18 14:46:05 -0700

    Sprint 6 challenge update.

--- src/index.js
+++ src/index.js
@@ -8,4 +8,31 @@ import "./App.css";
 import { worker } from "./mocks/browser";
 worker.start();
 
+function App() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        async function fetchCharacters() {
+            const response = await fetch('https://swapi.dev/api/people/');
+            const data = await response.json();
+            setCharacters(data.results);
+        }
+        retchCharacters();
+    }, []);
+
+    return (
+        <div>
+          {characters.map((character, index) => (
+            <div key={index}>
+              <h2>{character.name}</h2>
+              <p>Height: {character.height}</p>
+              <p>Mass: {character.mass}</p>
+            </div>
+          ))}
+        </div>
+    );
+}
+
+export default App;
+
 ReactDOM.render(<App />, document.getElementById("root"));
