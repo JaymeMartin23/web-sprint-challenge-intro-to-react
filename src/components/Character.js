8a91ffc46651b0e0593d258a63595120f99895c2
Jayme Martin <jaymelee70@gmail.com>
2023-04-21 15:56:56 -0700

    Sprint 6 challenge update.

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,19 +6,21 @@ const CharacterWrapper = styled.div`
   border: 1px solid #ccc;
   padding: 10px;
   margin-bottom: 10px;
+  text-align: center;
 `;
 
 const Name = styled.h2`
   margin-top: 0;
+  
 `;
 
 const Character = ({ name, height, gender, birthYear }) => {
   return (
     <CharacterWrapper>
       <Name>{name}</Name>
-      <p>Height: {height}</p>
-      <p>Gender: {gender}</p>
-      <p>Birth year: {birthYear}</p>
+      {/* <p>Height: {height}</p> */}
+      {/* <p>Gender: {gender}</p> */}
+      {/* <p>Birth year: {birthYear}</p> */}
     </CharacterWrapper>
   );
 };
